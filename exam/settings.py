c5765e2339e9811986289e9e53e1f75f4b521f69
Jure Cuhalev <gandalfar@gmail.com>
2012-11-01 16:06:22 +0100

    enable buildout

--- exam/settings.py
+++ exam/settings.py
@@ -23,30 +23,16 @@ SITE_ID = 1
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin-media/'
-MEDIA_ROOT = here('../media/')
+MEDIA_ROOT = 'exam/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'b)%xp225#p_3yvrqt38my7&r#ui=9g8@zidqmf)t9v!&cu4+gc'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-)
 
 ROOT_URLCONF = 'exam.urls'
 
 TEMPLATE_DIRS = (
-    here("../templates"),
+    'exam/templates',
 )
 
 INSTALLED_APPS = (
