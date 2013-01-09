6d7596e417f9fb9fca7655bfa1095d0df27a7b2a
Jure Cuhalev <gandalfar@gmail.com>
2013-01-09 12:03:30 +0100

    brown bag

--- exam/calc/management/commands/import_spss.py
+++ exam/calc/management/commands/import_spss.py
@@ -22,14 +22,13 @@ def main(options):
     # CalculatedAnswer.objects.all().delete()
     # return
 
-    for row in range(2, sh.nrows):
+    for row in range(2, 8):
         vpisna = int(sh.cell_value(row, 0))
         ime = sh.cell_value(row,2)
 
+        print vpisna
         var1_code = sh.cell_value(row, 3)
         var2_code = sh.cell_value(row, 5)
-
-        print var1_code
 
         var1 = Dataset.objects.get(varname=var1_code)
         var2 = Dataset.objects.get(varname=var2_code)
@@ -39,16 +38,18 @@ def main(options):
             profile = user.get_profile()
             profile.is_special = True
             profile.save()
-        else:
-            user = User.objects.create_user(vpisna, 'none@example.com', ime.lower())
-            profil = UserProfile(user=user, vpisna=vpisna, studijsko_leto='', izvajalec='',
-                       nacin_studija=0, cikel='', var1=var1, var2=var2, is_special=True)
-            profil.save()
 
-        var1.sel = '3, 6, 9, 12, 13, 17, 18, 21, 23, 24'
-        var1.save()
-        var1.sel = '3, 6, 9, 12, 13, 17, 18, 21, 23, 24'
-        var2.save()
+            print profile.is_special
+        # else:
+        #     user = User.objects.create_user(vpisna, 'none@example.com', ime.lower())
+        #     profil = UserProfile(user=user, vpisna=vpisna, studijsko_leto='', izvajalec='',
+        #                nacin_studija=0, cikel='', var1=var1, var2=var2, is_special=True)
+        #     profil.save()
+
+        # var1.sel = '3, 6, 9, 12, 13, 17, 18, 21, 23, 24'
+        # var1.save()
+        # var2.sel = '3, 6, 9, 12, 13, 17, 18, 21, 23, 24'
+        # var2.save()
 
         for q_id, val_no in [[12,7],[13,8],[14,9], [19,10], [20,11], [62, 12], [63,13], [51,14]]:
             q = Question.objects.get(pk=q_id)
