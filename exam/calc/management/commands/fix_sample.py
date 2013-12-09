6afa526874a1d3a25441bcb0220a26f53acd1776
Jure Cuhalev <gandalfar@gmail.com>
2013-12-08 20:14:55 -0500

    fix fix fix sample

--- exam/calc/management/commands/fix_sample.py
+++ exam/calc/management/commands/fix_sample.py
@@ -7,35 +7,34 @@ class Command(BaseCommand):
     help = "fixes country sample for 2013/14 year"
 
     def handle(self, *args, **options):
-        for var in Dataset.objects.all():
-            if var.varname in ['GEB1.2001', 'GEB1.2001-AS', 'PS25.2001', 'PS25.2001-AS']:
-                continue
+        # for var in Dataset.objects.all():
+        #     if var.varname in ['GEB1.2001', 'GEB1.2001-AS', 'PS25.2001', 'PS25.2001-AS']:
+        #         continue
 
-            if not var.userprofile_var1.count():
-                continue
+        #     if not var.userprofile_var1.count():
+        #         continue
 
-            # print var.userprofile_var1.all()
+        #     # print var.userprofile_var1.all()
             
-            sample = [int(i) for i in var.sel.split(',')]
-            sample.sort()
-            orig_sample = sample
+        #     sample = [int(i) for i in var.sel.split(',')]
+        #     sample.sort()
+        #     orig_sample = sample
 
-            sample = list(set(sample))
-            full_sample_count = len(var.get_sample_values())
+        #     sample = list(set(sample))
+        #     full_sample_count = len(var.get_sample_values())
 
-            if (len(sample) != 10) or (25 not in sample) or (full_sample_count != 10):
-                for vpisna in var.userprofile_var1.all():
-                    print '\t',vpisna
+        #     if (len(sample) != 10) or (25 not in sample) or (full_sample_count != 10):
+        #         for vpisna in var.userprofile_var1.all():
+        #             print '\t',vpisna
 
-                var.generate_sample()
-            # print sample
+        #         var.generate_sample()
+        #     # print sample
 
+        for var in Dataset.objects.filter(varname__endswith='-AS'):
+          if var.varname in ['GEB1.2001', 'GEB1.2001-AS', 'PS25.2001', 'PS25.2001-AS']:
+              continue
 
-        # for var in Dataset.objects.filter(varname__endswith='-AS'):
-        #   if var.varname in ['GEB1.2001', 'GEB1.2001-AS', 'PS25.2001', 'PS25.2001-AS']:
-        #       continue
+          print var.varname
 
-        #   print var.varname
-
-        #   var.sel = '3, 6, 9, 13, 14, 18, 19, 22, 24, 25'
-        #   var.save()
+          var.sel = '3, 6, 9, 13, 14, 18, 19, 22, 24, 25'
+          var.save()
