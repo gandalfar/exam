065c65060f3480bb4d602670a5d1f989db1c2af7
Jure Cuhalev <gandalfar@gmail.com>
2014-10-14 11:16:34 +0200

    update imports scripts

--- exam/calc/management/commands/import_data.py
+++ exam/calc/management/commands/import_data.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.core.management.base import BaseCommand
-from exam.calc.models import Lecture, Task, Section, Question, Answer, ExamLogEntry, Dataset, UserProfile
-from django.contrib.auth.models import User
-
 import sys
-import random, copy
-from pprint import pprint
 import xlrd
+
+from django.core.management.base import BaseCommand
+
+from exam.calc.models import Dataset
 
 def return_None(x):
     if x == '':
@@ -20,69 +18,71 @@ def check_index(x, idx):
     else:
       return ''
 
-def main(options):
-    wb = xlrd.open_workbook(sys.argv[2])
-    sh = wb.sheet_by_index(0)
-
-    for row in range(1, sh.nrows):
-        line = sh.row_values(row)
-        print line
-
-        args = {"region":line[0],
-                "varname":line[1],
-                "year":int(line[2]),
-                "descs":line[3],
-                "desce":line[5],
-                "c1":return_None(line[8]),
-                "c2":return_None(line[9]),
-                "c3":return_None(line[10]),
-                "c4":return_None(line[11]),
-                "c5":return_None(line[12]),
-                "c6":return_None(line[13]),
-                "c7":return_None(line[14]),
-                "c8":return_None(line[15]),
-                "c9":return_None(line[16]),
-                "c10":return_None(line[17]),
-                "c11":return_None(line[18]),
-                "c12":return_None(line[19]),
-                "c13":return_None(line[20]),
-                "c14":return_None(line[21]),
-                "c15":return_None(line[22]),
-                "c16":return_None(line[23]),
-                "c17":return_None(line[24]),
-                "c18":return_None(line[25]),
-                "c19":return_None(line[26]),
-                "c20":return_None(line[27]),
-                "c21":return_None(line[28]),
-                "c22":return_None(line[29]),
-                "c23":return_None(line[30]),
-                "c24":return_None(line[31]),
-                "c25":return_None(line[32]),
-                "c26":return_None(line[33]),
-                "c27":return_None(line[34]),
-                "c28":return_None(line[35]),
-                # "d1": check_index(line, 36),
-                # "u1": check_index(line, 37),
-                }
-
-        dataset_args = {}
-
-        for i in args:
-          if args[i]:
-            dataset_args[i] = args[i]
-
-        if Dataset.objects.filter(varname=args['varname'], year=args['year']).count():
-            p = Dataset.objects.get(varname=args['varname'], year=args['year'])
-        else:
-            p = Dataset(**args)
-
-        for x in args:
-           setattr(p, x, args[x])
-        p.save()
-        p.generate_sample()
-
 class Command(BaseCommand):
     help = "imports data in csv, tab delimited format"
 
     def handle(self, *args, **options):
-        main(options)
+        self.import_data()
+
+    def import_data(self):
+        wb = xlrd.open_workbook(sys.argv[2])
+
+        for idx in [0, 1]:
+            sh = wb.sheet_by_index(idx)
+
+            for row in range(1, sh.nrows):
+                line = sh.row_values(row)
+                print(line)
+
+                args = {"region":line[0],
+                        "varname":line[1],
+                        "year":int(line[2]),
+                        "descs":line[3],
+                        "desce":line[5],
+                        "c1":return_None(line[8]),
+                        "c2":return_None(line[9]),
+                        "c3":return_None(line[10]),
+                        "c4":return_None(line[11]),
+                        "c5":return_None(line[12]),
+                        "c6":return_None(line[13]),
+                        "c7":return_None(line[14]),
+                        "c8":return_None(line[15]),
+                        "c9":return_None(line[16]),
+                        "c10":return_None(line[17]),
+                        "c11":return_None(line[18]),
+                        "c12":return_None(line[19]),
+                        "c13":return_None(line[20]),
+                        "c14":return_None(line[21]),
+                        "c15":return_None(line[22]),
+                        "c16":return_None(line[23]),
+                        "c17":return_None(line[24]),
+                        "c18":return_None(line[25]),
+                        "c19":return_None(line[26]),
+                        "c20":return_None(line[27]),
+                        "c21":return_None(line[28]),
+                        "c22":return_None(line[29]),
+                        "c23":return_None(line[30]),
+                        "c24":return_None(line[31]),
+                        "c25":return_None(line[32]),
+                        "c26":return_None(line[33]),
+                        "c27":return_None(line[34]),
+                        "c28":return_None(line[35]),
+                        # "d1": check_index(line, 36),
+                        # "u1": check_index(line, 37),
+                        }
+
+                dataset_args = {}
+
+                for i in args:
+                  if args[i]:
+                    dataset_args[i] = args[i]
+
+                if Dataset.objects.filter(varname=args['varname'], year=args['year']).exists():
+                    p = Dataset.objects.get(varname=args['varname'], year=args['year'])
+                else:
+                    p = Dataset(**args)
+
+                for x in args:
+                   setattr(p, x, args[x])
+                p.save()
+                p.generate_sample()
--- exam/calc/management/commands/import_students.py
+++ exam/calc/management/commands/import_students.py
@@ -1,87 +1,94 @@
 # -*- coding: utf-8 -*-
-from django.core.management.base import BaseCommand
-from exam.calc.models import Lecture, Task, Section, Question, Answer, ExamLogEntry, Dataset, UserProfile
-from django.contrib.auth.models import User
-
 import sys
-import random, copy
-from pprint import pprint
 import xlrd
 from unidecode import unidecode
+
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from exam.calc.models import Dataset, UserProfile
+
+
 
 def return_None(x):
     if x == '':
       return 0.0
     else:
       return x
-      
-def main(options):
-    wb = xlrd.open_workbook(sys.argv[2])
-    sh = wb.sheet_by_index(0)
-
-    User.objects.filter(is_superuser=False).delete()
-
-    for row in range(1, sh.nrows):
-        ime = sh.cell_value(row, 2)
-        passwd = ime.lower()
-        passwd = unidecode(passwd)
-
-        priimek = sh.cell_value(row, 1)
-        vpisna = str(int(sh.cell_value(row, 0)))
-
-        print row, vpisna
-
-        if User.objects.filter(username__exact=vpisna):
-          print "Uporabnik %s ze obstaja v bazi" % vpisna
-          user = User.objects.get(username__exact=vpisna)
-          user.set_password(passwd)
-        else:
-          user = User.objects.create_user(vpisna, 'none@example.com', passwd)
-        
-        user.is_active = True
-        user.first_name = ime
-        user.last_name  = priimek
-        user.save()
-        
-        studijsko_leto = sh.cell_value(row,7)
-        izvajalec = sh.cell_value(row,6)
-
-        raw_tip_studija = sh.cell_value(row,3)
-
-        if raw_tip_studija == 'redni':
-            nacin_studija = 0
-        elif raw_tip_studija == 'izredni':
-            nacin_studija = 1
-        elif raw_tip_studija == 'Stari program':
-            nacin_studija = 2
-        else:
-            nacin_studija = 3
-
-        var1 = sh.cell_value(row, 8)
-        year1 = sh.cell_value(row,9)
-
-        var2 = sh.cell_value(row,10)
-        year2 = sh.cell_value(row,11)
-
-        var1 = Dataset.objects.get(varname__exact=var1)
-        var2 = Dataset.objects.get(varname__exact=var2)
-        
-
-        UserProfile.objects.get_or_create(
-            user = user,
-            vpisna = vpisna,
-            defaults = {
-                'studijsko_leto': studijsko_leto, 
-                'izvajalec': izvajalec,
-                'nacin_studija': nacin_studija, 
-                'cikel': '', 
-                'var1': var1, 
-                'var2': var2            
-            }
-        )    
 
 class Command(BaseCommand):
     help = "imports users in csv, tab delimited format"
 
+    option_list = BaseCommand.option_list + (
+        make_option("--delete", action="store_true", dest="delete", help="Deletes all data"),
+    )
+
     def handle(self, *args, **options):
-      main(options)
+        if options.get('delete'):
+            self.stdout.write("Deleted {0} users".format(User.objects.filter(is_superuser=False).count()))
+            User.objects.filter(is_superuser=False).delete()
+        else:
+            self.import_students()
+      
+    def import_students(options):
+        wb = xlrd.open_workbook(sys.argv[2])
+        sh = wb.sheet_by_index(0)
+
+        for row in range(1, sh.nrows):
+            ime = sh.cell_value(row, 2)
+            passwd = ime.lower()
+            passwd = unidecode(passwd)
+
+            priimek = sh.cell_value(row, 1)
+            vpisna = str(int(sh.cell_value(row, 0)))
+
+            print(row, vpisna)
+
+            if User.objects.filter(username__exact=vpisna):
+              print("Uporabnik %s ze obstaja v bazi" % vpisna)
+              user = User.objects.get(username__exact=vpisna)
+              user.set_password(passwd)
+            else:
+              user = User.objects.create_user(vpisna, 'none@example.com', passwd)
+            
+            user.is_active = True
+            user.first_name = ime
+            user.last_name  = priimek
+            user.save()
+            
+            studijsko_leto = sh.cell_value(row,7)
+            izvajalec = sh.cell_value(row,6)
+
+            raw_tip_studija = sh.cell_value(row,3)
+
+            if raw_tip_studija == 'redni':
+                nacin_studija = 0
+            elif raw_tip_studija == 'izredni':
+                nacin_studija = 1
+            elif raw_tip_studija == 'Stari program':
+                nacin_studija = 2
+            else:
+                nacin_studija = 3
+
+            var1 = sh.cell_value(row, 8)
+            # year1 = sh.cell_value(row,9)
+
+            var2 = sh.cell_value(row,10)
+            # year2 = sh.cell_value(row,11)
+
+            var1 = Dataset.objects.get(varname__exact=var1)
+            var2 = Dataset.objects.get(varname__exact=var2)            
+
+            UserProfile.objects.get_or_create(
+                user = user,
+                vpisna = vpisna,
+                defaults = {
+                    'studijsko_leto': studijsko_leto, 
+                    'izvajalec': izvajalec,
+                    'nacin_studija': nacin_studija, 
+                    'cikel': '', 
+                    'var1': var1, 
+                    'var2': var2            
+                }
+            )    
